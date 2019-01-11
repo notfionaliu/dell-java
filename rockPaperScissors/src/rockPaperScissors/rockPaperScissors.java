ad4a01629fa1291f2372e396e29202beff6a27de
Liu <Fiona_Liu@Dell.com>
2019-01-11 15:32:34 -0600

    rockpaper scissors submission

--- rockPaperScissors/src/rockPaperScissors/rockPaperScissors.java
+++ rockPaperScissors/src/rockPaperScissors/rockPaperScissors.java
@@ -5,18 +5,25 @@ import java.util.Random;
 
 public class rockPaperScissors {
 	public static void main(String[] args) {
-		System.out.println("What's your hand?");
+		System.out.println("What's your hand? (rock paper scissor)");
 		Scanner scan = new Scanner(System.in);
 		String userInput = scan.nextLine();
 		scan.close();
 		String computer = shoot();
+		System.out.println("Computer's hand is: " + computer); 
+//		debugging.... shoot correctly returns a value
 		String winner = returnWinner(userInput, computer);
+//		System.out.println(winner);
 		if (winner == "tie") {
 			System.out.println("It's a tie!");
 		}
 		else
 			System.out.println("The winner is " + winner );
 	}
+	
+	
+//	method randomly chooses a number between 1 and 3
+//	method returns a String of a corresponding output 
 	
 	public static String shoot() {
 		Random randNumberGenerator = new Random();
@@ -37,29 +44,30 @@ public class rockPaperScissors {
 	public static String returnWinner(String user, String computer) {
 		String output = null;
 		
-		if (user == computer)
+		if (user.equals(computer))
 			output = "tie";
-		if (user == "rock") {
-			if (computer == "paper") {
+		else if (user.equals("rock")) {
+			if (computer.equals("paper")) {
 				output = "computer";
 			}
 			else
 				output = "user";
 			}
-		if (user == "paper") {
-			if (computer == "rock") {
-				output = "computer";
-			}
-			else
-				output = "user";
-		}
-		if (user == "scissor") {
-			if (computer == "rock") {
+		else if (user.equals("paper")) {
+			if (computer.equals("rock")) {
 				output = "computer";
 			}
 			else
 				output = "user";
 		}
+		else if (user.equals("scissor")){
+			if (computer.equals("rock")) {
+				output = "computer";
+			}
+			else
+				output = "user";
+		}
+		System.out.println(output);
 		return output;
 	}
 
